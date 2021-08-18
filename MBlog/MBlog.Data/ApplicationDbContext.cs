da62dbf25c6a16af89eb2b38ed173747cf5d3924
Muhammed <muhammed.ogur@gmail.com>
2021-08-18 13:35:48 +0300

    Next step will be EF migrations

---
+++ MBlog/MBlog.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MBlog.Data.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MBlog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ MBlog/MBlog.Web/Configuration/AppServices.cs
@@ -0,0 +1,22 @@
+﻿using MBlog.Data;
+using MBlog.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MBlog.Web.Configuration
+{
+    public static class AppServices
+    {
+        public static void AddDefaultServices(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+    }
+}
